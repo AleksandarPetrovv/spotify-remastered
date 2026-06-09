ef1b7a67aba65f9a9b2c7d8fa35bba07126aee82
AleksandarPetrovv <azpetrovv@gmail.com>
2026-06-09 06:13:44 +0300

    added remade hazy theme

---
+++ hazy/theme.js
@@ -0,0 +1,6 @@
+(() => {
+    const script = document.createElement("SCRIPT");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("src", "https://cdn.jsdelivr.net/gh/astromations/hazy/hazy.js");
+    document.head.appendChild(script);
+})();

fe077768139b2f901e49ad9b12c1a684277d9387
AleksandarPetrovv <AlekPetrov2009@gmail.com>
2026-06-10 02:15:53 +0300

    default the brightness to 50

--- hazy/theme.js
+++ hazy/theme.js
@@ -1,4 +1,7 @@
 (() => {
+    if (localStorage.getItem("brightAmount") === null) {
+        localStorage.setItem("brightAmount", "50");
+    }
     const script = document.createElement("SCRIPT");
     script.setAttribute("type", "text/javascript");
     script.setAttribute("src", "https://cdn.jsdelivr.net/gh/astromations/hazy/hazy.js");
